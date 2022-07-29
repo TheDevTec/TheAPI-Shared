4abf0b841bf43e77144efd8439aedbf92173d699
StraikerinaCZ <Straiker123@email.cz>
2022-07-29 17:54:19 +0200

    Update reverted

--- Shared/src/me/devtec/shared/utility/StreamUtils.java
+++ Shared/src/me/devtec/shared/utility/StreamUtils.java
@@ -1,15 +1,11 @@
 package me.devtec.shared.utility;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
-import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
-import java.nio.channels.Channels;
-import java.nio.channels.FileChannel;
-import java.nio.channels.ReadableByteChannel;
-import java.nio.charset.CharsetDecoder;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.StandardOpenOption;
 
 public class StreamUtils {
 
@@ -19,41 +15,10 @@ public class StreamUtils {
 	 */
 	public static String fromStream(File file) {
 		try {
-			FileChannel channel = FileChannel.open(file.toPath(), StandardOpenOption.READ);
-			StringBuilder out = new StringBuilder();
-
-			int bufferSize = 4096;
-			if (bufferSize > channel.size())
-				bufferSize = (int) channel.size();
-			ByteBuffer buff = ByteBuffer.allocate(bufferSize);
-			while (channel.read(buff) > 0) {
-				out.append(toUTF8(buff));
-				buff.clear();
-			}
-			return out.toString();
+			return fromStream(new FileInputStream(file));
 		} catch (Exception err) {
 			return null;
 		}
-	}
-
-	/**
-	 * @apiNote Wrap byte[] to ByteBuffer and decode ByteBuffer bytes with UTF8
-	 *          Decoder
-	 * @return char[]
-	 */
-	public static char[] toUTF8(byte[] bytes) {
-		return toUTF8(ByteBuffer.wrap(bytes));
-	}
-
-	/**
-	 * @apiNote Decode ByteBuffer bytes with UTF8 Decoder
-	 * @return char[]
-	 */
-	public static char[] toUTF8(ByteBuffer buff) {
-		CharsetDecoder cd = StandardCharsets.UTF_8.newDecoder();
-		CharBuffer buf = CharBuffer.allocate(buff.array().length);
-		cd.decode(buff, buf, true);
-		return buf.array();
 	}
 
 	/**
@@ -62,16 +27,15 @@ public class StreamUtils {
 	 */
 	public static String fromStream(InputStream stream) {
 		try {
-			ReadableByteChannel channel = Channels.newChannel(stream);
-			StringBuilder out = new StringBuilder();
-
-			int bufferSize = 2048;
-			ByteBuffer buff = ByteBuffer.allocate(bufferSize);
-			while (channel.read(buff) > 0) {
-				out.append(toUTF8(buff));
-				buff.clear();
+			BufferedReader br = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8), 4096);
+			StringBuilder sb = new StringBuilder(512);
+			String content;
+			while ((content = br.readLine()) != null) {
+				if (sb.length() != 0)
+					sb.append(System.lineSeparator());
+				sb.append(content);
 			}
-			return out.toString();
+			return sb.toString();
 		} catch (Exception err) {
 			return null;
 		}
