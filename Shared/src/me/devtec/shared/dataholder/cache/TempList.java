5960303d56d9df5fd66ddb50c4c1319dd144e68c
StraikerinaCZ <Straiker123@email.cz>
2022-10-24 00:01:55 +0200

    Prevent Iterator from updating values

--- Shared/src/me/devtec/shared/dataholder/cache/TempList.java
+++ Shared/src/me/devtec/shared/dataholder/cache/TempList.java
@@ -3,9 +3,12 @@ package me.devtec.shared.dataholder.cache;
 import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Map.Entry;
+import java.util.NoSuchElementException;
 
 import me.devtec.shared.dataholder.StringContainer;
 import me.devtec.shared.scheduler.Tasker;
@@ -136,5 +139,143 @@ public class TempList<V> extends AbstractList<V> {
 			container.append(iterator.next().getKey() + "");
 		}
 		return container.append(']').toString();
+	}
+
+	@Override
+	public Iterator<V> iterator() {
+		return new Itr();
+	}
+
+	@Override
+	public ListIterator<V> listIterator(int index) {
+		if (index < 0 || index > size())
+			throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size());
+		return new ListItr(index);
+	}
+
+	private class Itr implements Iterator<V> {
+		/**
+		 * Index of element to be returned by subsequent call to next.
+		 */
+		int cursor = 0;
+
+		/**
+		 * Index of element returned by most recent call to next or previous. Reset to
+		 * -1 if this element is deleted by a call to remove.
+		 */
+		int lastRet = -1;
+
+		/**
+		 * The modCount value that the iterator believes that the backing List should
+		 * have. If this expectation is violated, the iterator has detected concurrent
+		 * modification.
+		 */
+		int expectedModCount = modCount;
+
+		@Override
+		public boolean hasNext() {
+			return cursor != size();
+		}
+
+		@Override
+		public V next() {
+			checkForComodification();
+			try {
+				int i = cursor;
+				Entry<V, Long> next = getRaw(i);
+				lastRet = i;
+				cursor = i + 1;
+				return next.getKey();
+			} catch (IndexOutOfBoundsException e) {
+				checkForComodification();
+				throw new NoSuchElementException();
+			}
+		}
+
+		@Override
+		public void remove() {
+			if (lastRet < 0)
+				throw new IllegalStateException();
+			checkForComodification();
+
+			try {
+				TempList.this.remove(lastRet);
+				if (lastRet < cursor)
+					cursor--;
+				lastRet = -1;
+				expectedModCount = modCount;
+			} catch (IndexOutOfBoundsException e) {
+				throw new ConcurrentModificationException();
+			}
+		}
+
+		final void checkForComodification() {
+			if (modCount != expectedModCount)
+				throw new ConcurrentModificationException();
+		}
+	}
+
+	private class ListItr extends Itr implements ListIterator<V> {
+		ListItr(int index) {
+			cursor = index;
+		}
+
+		@Override
+		public boolean hasPrevious() {
+			return cursor != 0;
+		}
+
+		@Override
+		public V previous() {
+			checkForComodification();
+			try {
+				int i = cursor - 1;
+				V previous = get(i);
+				lastRet = cursor = i;
+				return previous;
+			} catch (IndexOutOfBoundsException e) {
+				checkForComodification();
+				throw new NoSuchElementException();
+			}
+		}
+
+		@Override
+		public int nextIndex() {
+			return cursor;
+		}
+
+		@Override
+		public int previousIndex() {
+			return cursor - 1;
+		}
+
+		@Override
+		public void set(V e) {
+			if (lastRet < 0)
+				throw new IllegalStateException();
+			checkForComodification();
+
+			try {
+				TempList.this.set(lastRet, e);
+				expectedModCount = modCount;
+			} catch (IndexOutOfBoundsException ex) {
+				throw new ConcurrentModificationException();
+			}
+		}
+
+		@Override
+		public void add(V e) {
+			checkForComodification();
+
+			try {
+				int i = cursor;
+				TempList.this.add(i, e);
+				lastRet = -1;
+				cursor = i + 1;
+				expectedModCount = modCount;
+			} catch (IndexOutOfBoundsException ex) {
+				throw new ConcurrentModificationException();
+			}
+		}
 	}
 }
