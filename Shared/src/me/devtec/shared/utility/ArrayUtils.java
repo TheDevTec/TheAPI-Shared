9c4307436d01cb7ee60eb54f2baeff4953f86d68
StraikerinaCZ <Straiker123@email.cz>
2023-04-02 17:06:30 +0200

    Added new insert methods

--- Shared/src/me/devtec/shared/utility/ArrayUtils.java
+++ Shared/src/me/devtec/shared/utility/ArrayUtils.java
@@ -45,22 +45,57 @@ public class ArrayUtils {
 	/**
 	 * Move item at fromPos to the specified fromPosition
 	 */
-	public static void move(Object[] array, int fromPos, int toPos) {
+	public static void move(Object array, int fromPos, int toPos) {
 		if (fromPos == toPos)
 			return; // Why are you moving to the same pos?
 
-		Object val = array[fromPos];
+		Object val = Array.get(array, fromPos);
 
 		// before fromPos
 		System.arraycopy(array, 0, array, 0, fromPos);
 
 		// to the fromPos
-		System.arraycopy(array, fromPos + (fromPos < toPos ? 1 : 0), array, fromPos, array.length - 1 - fromPos);
+		System.arraycopy(array, fromPos + (fromPos < toPos ? 1 : 0), array, fromPos, Array.getLength(array) - 1 - fromPos);
 
 		// after toPos
-		System.arraycopy(array, toPos, array, toPos + 1, array.length - 1 - toPos);
+		System.arraycopy(array, toPos, array, toPos + 1, Array.getLength(array) - 1 - toPos);
 
 		// set value at toPos
-		array[toPos] = val;
+		Array.set(array, toPos, val);
+	}
+
+	/**
+	 * Move array values between specified position to the "last" position
+	 */
+	public static void move(Object array, int from, int to, int length) {
+		if (from < 0 || from > to)
+			throw new IndexOutOfBoundsException("Array length is " + length + ", moving from position " + from + " to " + to);
+		System.arraycopy(array, from, array, to, length - from);
+	}
+
+	/**
+	 * Insert value on certain position
+	 */
+	public static void insert(Object array, int position, Object value, int length) {
+		if (position < 0 || position >= length)
+			throw new IndexOutOfBoundsException("Array length is " + (length - 1) + ", insert position is " + position);
+		System.arraycopy(array, position, array, position + 1, length - position - 1);
+		Array.set(array, position, value);
+	}
+
+	/**
+	 * Insert value on certain position and return new array
+	 */
+	public static Object insert(Object array, int position, Object value) {
+		int length = Array.getLength(array) + 1;
+		if (position < 0 || position >= length)
+			throw new IndexOutOfBoundsException("Array length is " + (length - 1) + ", insert position is " + position);
+		if (position >= length)
+			position = length + 1;
+		Object newInstance = array.getClass().getComponentType() == Object[].class ? new Object[length] : Array.newInstance(array.getClass().getComponentType(), length);
+		System.arraycopy(array, 0, newInstance, 0, position);
+		System.arraycopy(array, position, newInstance, position + 1, length - position - 1);
+		Array.set(newInstance, position, value);
+		return newInstance;
 	}
 }
